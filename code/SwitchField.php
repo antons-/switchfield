92fc9133ab6ba82edafa335f071c8b86a55da83c
Anton Smith <Anton.Smith@op.ac.nz>
2016-02-18 13:59:26 +1300

    Initial commit

---
+++ code/SwitchField.php
@@ -0,0 +1,29 @@
+<?php
+
+class SwitchField extends CheckboxField {
+
+	public function Field($properties = array()) {
+		Requirements::css('switchfield/thirdparty/switchery/switchery.min.css');
+
+		Requirements::javascript('switchfield/thirdparty/switchery/switchery.min.js');
+		Requirements::javascript('switchfield/javascript/SwitchField.js');
+
+		return parent::Field($properties);
+	}
+
+	public function performReadonlyTransformation() {
+		$field = new SwitchField_Readonly($this->name, $this->title, $this->value);
+		$field->setForm($this->form);
+		return $field;
+	}
+}
+
+class SwitchField_Readonly extends CheckboxField_Readonly {
+
+	public function Value() {
+		return Convert::raw2xml($this->value ?
+			_t('SwitchField.YESANSWER', 'Yes') :
+			_t('SwitchField.NOANSWER', 'No'));
+	}
+
+}
---
+++ javascript/SwitchField.js
@@ -0,0 +1,9 @@
+﻿(function ($) {
+    $.entwine(function ($) {
+        $('.cms-edit-form .field.switch').entwine({
+            onmatch: function () {
+                new Switchery(this.find('.middleColumn input[type=checkbox]')[0]);
+            }
+        });
+    });
+})(jQuery);
